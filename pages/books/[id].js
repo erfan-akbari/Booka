d7c70a8da63bf0afd64256e6c7ea22c330ce0539
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-10-30 09:23:29 +0330

    Project installation

---
+++ pages/books/[id].js
@@ -0,0 +1,13 @@
+import Router from "next/router"
+
+function BookDetails() {
+
+    const params = Router()
+    console.log(params);
+    
+  return (
+    <div>BookDetails</div>
+  )
+}
+
+export default BookDetails

47a910a7d1bdde2522badcf401a5ec657913e282
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-10-30 13:29:25 +0330

    aded library

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import Footer from '@/components/template/Footer'
+import Header from '@/components/template/Header'
 import '@/styles/globals.css'
 
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 
-
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
-    // <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
     <>
       <div className="">
         <h1 className="text-3xl text-orange-600">Booka</h1>
       </div>
-
     </>
   )
 }

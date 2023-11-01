7606a8262dd88dccc3446b2b965843084b5f9e1b
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-01 22:15:19 +0330

    coding SectionBookDetails

--- components/module/CardV2.jsx
+++ components/module/CardV2.jsx
@@ -1,16 +1,16 @@
 import Link from 'next/link'
 import React from 'react'
 
-function CardV2({ poster, title, price, score, shadow, color }) {
+function CardV2({ id, poster, title, price, score, shadow, color }) {
     return (
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
+            <Link href={`/books/${id}`}>
                 <img className="p-8 rounded-t-lg" src={poster} alt="product image" />
-            </a>
+            </Link>
             <div className="px-5 pb-5">
-                <a href="#">
+                <Link href={`/books/${id}`}>
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h5>
-                </a>
+                </Link>
                 <div className="flex items-center mt-2.5 mb-5">
                     <svg className="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
                         <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
@@ -31,7 +31,7 @@ function CardV2({ poster, title, price, score, shadow, color }) {
                 </div>
                 <div className="flex items-center justify-between">
                     <span className="text-base font-bold text-gray-900 dark:text-white">تومان {price}</span>
-                    <Link href="#" className={`text-white bg-gradient-to-t ${color} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>
+                    <Link href={`/books/${id}`} className={`text-white bg-gradient-to-t ${color} hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}>
                         مشاهد کتاب
                     </Link>
                 </div>

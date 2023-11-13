282660475e5c4af269e0806a3e2c0976eb3cd5a7
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-13 21:07:30 +0330

    Complete the users route

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,51 +1,31 @@
 import connectToDB from '@/utils/db'
 import usersModel from "@/models/user"
 
-const fs = require('fs')
-const path = require('path')
-
 const handler = async (req, res) => {
     connectToDB()
     const { method } = req
     const { username, password, email } = req.body
 
-    const dbPath = path.join(process.cwd(), 'Data', 'db.json')
-    const data = fs.readFileSync(dbPath)
-    const parsedData = JSON.parse(data)
-
     switch (method) {
         case 'GET': {
             const users = await usersModel.find({})
-            res.status(200).json(...users)
+            res.status(200).json(users)
             break;
         }
-
         case 'POST': {
-            const isInUser = parsedData.users.some(user => user.username === username)
-
-            if (!isInUser && (username && password && email)) {
-                parsedData.users.push({
-                    id: crypto.randomUUID(),
-                    username,
-                    password,
-                    email,
-                })
-
-                const err = fs.writeFileSync(dbPath, JSON.stringify(parsedData))
-
-                if (err) {
-                    console.log('erorr =>', err);
+            const isInUser = await usersModel.findOne({ username })
+            if (!isInUser?.username && username && password && email) {
+                const user = await usersModel.create({ username, password, email })
+                if (user) {
+                    return res.status(201).json({ message: 'SuccessFully Register', user })
                 } else {
-                    res.status(201).json(...parsedData.users)
+                    return res.status(409).json({ message: 'Failed to register user' })
                 }
             } else {
-                res.status(422).json({
-                    message: 'error in body request'
-                })
+                return res.status(422).json({ message: 'error in body request' })
             }
             break;
         }
-
         default: {
             res.json({ message: 'not found' })
             break;

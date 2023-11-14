f69a553b1d15d95a2e1f2e4fc2b473d94c32f8bf
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-14 21:37:17 +0330

    Development model topRatedBooks & api route

--- pages/api/topRatedBooks/index.js
+++ pages/api/topRatedBooks/index.js
@@ -1,18 +1,18 @@
-const fs = require('fs')
-const path = require('path')
+import connectToDB from "@/utils/db"
+import topRatedBooksModel from "@/models/topRatedBooks"
 
-const handler = (req, res) => {
-    const dbAddress = path.join(process.cwd(), 'Data', 'db.json')
-    const data = fs.readFileSync(dbAddress)
-    const parsedData = JSON.parse(data)
-
+const handler = async (req, res) => {
+    connectToDB()
     if (req.method === 'GET') {
-        return res
-        .status(200)
-        .json(parsedData.topRatedBooks)
+        const topRatedBooks = await topRatedBooksModel.find()
+        if (topRatedBooks) {
+            return res.status(200).json(topRatedBooks)
+        } else {
+            return res.status(500).json({ message: 'server error' })
+        }
     }
-    
-    return res.json({ message: 'not found' })
+
+    return res.json({ message: 'http method not found' })
 }
 
 export default handler

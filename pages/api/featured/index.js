740f24f476951e1cb9bd7283f65d82eef0c38008
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-14 21:36:22 +0330

    Development model featured & api route

--- pages/api/featured/index.js
+++ pages/api/featured/index.js
@@ -1,18 +1,19 @@
-const fs = require('fs')
-const path = require('path')
+import connectToDB from "@/utils/db"
+import featuredModel from "@/models/featured"
 
-const handler = (req, res) => {
-    const dbAddress = path.join(process.cwd(), 'Data', 'db.json')
-    const data = fs.readFileSync(dbAddress)
-    const parsedData = JSON.parse(data)
+const handler = async (req, res) => {
+    connectToDB()
 
     if (req.method === 'GET') {
-        return res
-        .status(200)
-        .json(parsedData.featured)
+        try {
+            const featured = await featuredModel.find()
+            return res.status(200).json(featured)
+        } catch (error) {
+            return res.status(500).json({ message: 'server error' })
+        }
     }
-    
-    return res.json({ message: 'not found' })
+
+    return res.json({ message: 'http method not found' })
 }
 
 export default handler

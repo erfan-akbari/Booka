73700815eebfec0a1e6a6086cf7f3ebcd2805860
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-15 19:04:58 +0330

    Added new page route topRated

---
+++ pages/books/topRated/[id].js
@@ -0,0 +1,57 @@
+import Carousel from "@/components/template/Carousel";
+import SectionBookDetails from "@/components/template/SectionBookDetails";
+import ContentWrapper from "@/components/module/ContentWrapper";
+import Tab from "@/components/template/Tab";
+
+import connectToDB from "@/utils/db";
+import topRatedBooksModel from "@/models/topRatedBooks"
+import featuredModel from "@/models/featured"
+
+
+export default function BookDetails({ book, featured }) {
+  return (
+    <main className="shadow-inner">
+      <SectionBookDetails data={...book} />
+      <ContentWrapper>
+        <Tab />
+        <Carousel data={featured} title={'محصولات مرتبط'} type={'featured'} />
+      </ContentWrapper>
+    </main>
+  )
+}
+
+export async function getStaticPaths() {
+  connectToDB()
+  const books = await topRatedBooksModel.find()
+
+  const paths = books?.slice(0, 3).map(book => {
+    return {
+      params: { id: String(book._id) }
+    }
+  })
+
+  return {
+    paths,
+    fallback: "blocking",
+  }
+}
+
+export async function getStaticProps({ params }) {
+  connectToDB()
+  const book = await topRatedBooksModel.findOne({ _id: params.id })
+  const featured = await featuredModel.find()
+console.log(book);
+  if (!book) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: {
+      book: JSON.parse(JSON.stringify(book)),
+      featured: JSON.parse(JSON.stringify(featured)),
+    },
+    revalidate: 60 * 60 * 12 // 43,200
+  }
+}

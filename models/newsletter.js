e1e912067a9a7b24b33fa80cf0d85ed01731d5fd
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-16 19:07:30 +0330

    coding newsletter model & schema

---
+++ models/newsletter.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+    email: {
+        type: String,
+    }
+})
+
+const model = mongoose.models.newsletter || mongoose.model('newsletter', schema)
+
+export default model;

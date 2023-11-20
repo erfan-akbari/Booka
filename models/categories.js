f747f0364394e294ceefeae79401ec88ccfcd647
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-20 21:43:54 +0330

    Development categories model

--- models/categories.js
+++ models/categories.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema({
     title: {
         type: String
-    }
+    },
+    route: {
+        type: String
+    },
 })
 
-const model = mongoose.models.categories || mongoose.model('categories', schema)
+const model = mongoose.models?.categories || mongoose.model('categories', schema)
 
 export default model;

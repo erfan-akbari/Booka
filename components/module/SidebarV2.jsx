dd9dfc09eacd4e77f9acf1c851325b5c9a3294c5
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-22 21:11:37 +0330

    Development filters Data & styles

--- components/module/SidebarV2.jsx
+++ components/module/SidebarV2.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     Accordion,
     AccordionHeader,
     AccordionBody,
 } from "@material-tailwind/react";
+import { TiTick } from "react-icons/ti";
+import Link from "next/link";
 
 function Icon({ id, open }) {
     return (
@@ -20,8 +22,16 @@ function Icon({ id, open }) {
     );
 }
 
-function SidebarV2({ categories }) {
-    const [open, setOpen] = React.useState(0);
+function SidebarV2({ data }) {
+    const { categories, writers, translators, publications } = data
+
+    const [open, setOpen] = useState(1);
+    const [categorieFilter, setCategorieFilter] = useState("همه");
+    const [publicationFilter, setPublicationFilter] = useState("همه");
+    const [writerFilter, setWriterFilter] = useState("همه");
+    const [translatorFilter, setTranslatorFilter] = useState("همه");
+
+
     const handleOpen = (value) => setOpen(open === value ? 0 : value);
 
     return (
@@ -31,57 +41,107 @@ function SidebarV2({ categories }) {
             <Accordion open={open === 1} icon={<Icon id={1} open={open} />}>
                 <AccordionHeader onClick={() => handleOpen(1)}>دسته بندی</AccordionHeader>
                 <AccordionBody>
-                    <ul>
-                        {categories.map(cat => (
-                            <li key={cat._id}>
-                                {cat.text}
-                            </li>
+                    <ul className='flex flex-col gap-2'>
+                        <Link
+                            onClick={() => setCategorieFilter('همه')}
+                            href={`/books`}
+                            className={`hover:text-rose-800 cursor-pointer flex items-center ${categorieFilter === 'همه' && 'text-rose-800 font-bold'}`}
+                        >
+                            همه
+                            {categorieFilter === 'همه' && <TiTick className="text-lime-500 text-xl" />}
+                        </Link>
+                        {categories?.map(item => (
+                            <Link
+                                onClick={() => setCategorieFilter(item.text)}
+                                href={`/books?cat=${item.text}`}
+                                className={`hover:text-rose-800 cursor-pointer flex items-center ${item.text === categorieFilter && 'text-rose-800 font-bold'}`}
+                                key={item._id}>
+                                {item.text}
+                                {item.text === categorieFilter && <TiTick className="text-lime-500 text-xl" />}
+                            </Link>
                         ))}
                     </ul>
                 </AccordionBody>
             </Accordion>
             <Accordion open={open === 2} icon={<Icon id={2} open={open} />}>
                 <AccordionHeader onClick={() => handleOpen(2)}>
-                    گرونه سنی
+                    انتشارات
                 </AccordionHeader>
                 <AccordionBody>
-                    <ul>
-                        <li>آقایان</li>
-                        <li>بانوان</li>
-                        <li>جوانان</li>
-                        <li>نوجوانان</li>
-                        <li>کودکان</li>
+                    <ul className='flex flex-col gap-2'>
+                        <Link
+                            onClick={() => setPublicationFilter('همه')}
+                            href={`/books`}
+                            className={`hover:text-rose-800 cursor-pointer flex items-center ${publicationFilter === 'همه' && 'text-rose-800 font-bold'}`}
+                        >
+                            همه
+                            {publicationFilter === 'همه' && <TiTick className="text-lime-500 text-xl" />}
+                        </Link>
+                        {publications?.map(item => (
+                            <Link
+                                onClick={() => setPublicationFilter(item.title)}
+                                href={`/books?p=${item.title}`}
+                                className={`hover:text-rose-800 cursor-pointer flex items-center ${item.title === publicationFilter && 'text-rose-800 font-bold'}`}
+                                key={item._id}>
+                                {item.title}
+                                {item.title === publicationFilter && <TiTick className="text-lime-500 text-xl" />}
+                            </Link>
+                        ))}
                     </ul>
                 </AccordionBody>
             </Accordion>
             <Accordion open={open === 3} icon={<Icon id={3} open={open} />}>
                 <AccordionHeader onClick={() => handleOpen(3)}>
-                    سایز کتاب (قطع)
+                    نویسنده
                 </AccordionHeader>
                 <AccordionBody>
-                    <ul>
-                        <li>وزیری</li>
-                        <li>رقعی</li>
-                        <li>رحلی</li>
-                        <li>خشتی</li>
-                        <li>سلطانی</li>
-                        <li>جیبی</li>
+                    <ul className='flex flex-col gap-2'>
+                        <Link
+                            onClick={() => setWriterFilter('همه')}
+                            href={`/books`}
+                            className={`hover:text-rose-800 cursor-pointer flex items-center ${writerFilter === 'همه' && 'text-rose-800 font-bold'}`}
+                        >
+                            همه
+                            {writerFilter === 'همه' && <TiTick className="text-lime-500 text-xl" />}
+                        </Link>
+                        {writers?.map(item => (
+                            <Link
+                                onClick={() => setWriterFilter(item.title)}
+                                href={`/books?w=${item.title}`}
+                                className={`hover:text-rose-800 cursor-pointer flex items-center ${item.title === writerFilter && 'text-rose-800 font-bold'}`}
+                                key={item._id}>
+                                {item.title}
+                                {item.title === writerFilter && <TiTick className="text-lime-500 text-xl" />}
+                            </Link>
+                        ))}
                     </ul>
                 </AccordionBody>
             </Accordion>
             <Accordion open={open === 4} icon={<Icon id={4} open={open} />}>
                 <AccordionHeader onClick={() => handleOpen(4)}>
-                    ناشران
+                    مترجم
                 </AccordionHeader>
                 <AccordionBody>
-                    <ul>
-                        <li className="box">ناشر یک</li>
-                        <li className="box">ناشر دو</li>
-                        <li className="box">ناشر سه</li>
-                        <li className="box">ناشر چهار</li>
-                        <li className="box">ناشر پنج</li>
-                        <li className="box">ناشر شش</li>
-                    </ul >
+                    <ul className='flex flex-col gap-2'>
+                        <Link
+                            onClick={() => setTranslatorFilter('همه')}
+                            href={`/books`}
+                            className={`hover:text-rose-800 cursor-pointer flex items-center ${translatorFilter === 'همه' && 'text-rose-800 font-bold'}`}
+                        >
+                            همه
+                            {translatorFilter === 'همه' && <TiTick className="text-lime-500 text-xl" />}
+                        </Link>
+                        {translators?.map(item => (
+                            <Link
+                                onClick={() => setTranslatorFilter(item.title)}
+                                href={`/books?t=${item.title}`}
+                                className={`hover:text-rose-800 cursor-pointer flex items-center ${item.title === translatorFilter && 'text-rose-800 font-bold'}`}
+                                key={item._id}>
+                                {item.title}
+                                {item.title === translatorFilter && <TiTick className="text-lime-500 text-xl" />}
+                            </Link>
+                        ))}
+                    </ul>
                 </AccordionBody >
             </Accordion >
             <ul>

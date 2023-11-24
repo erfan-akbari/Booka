b3cc8529ad062a33f53bb6b2d67b0da4496e3ffb
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-24 13:44:34 +0330

    Create New Component ScoreBox

---
+++ components/module/ScoreBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function ScoreBox({ score }) {
+    return (
+        <div className='flex items-center gap-1'>
+            {
+                new Array(5 - score).fill('star').map((star, i) => (
+                    <svg key={i + 1} className="w-4 h-4 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                ))
+            }
+            {
+                new Array(score).fill('starFill').map((starFill, i) => (
+                    <svg key={i + 1} className="w-4 h-4 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                    </svg>
+                ))
+            }
+             <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{score}</span>
+        </div>
+    )
+}
+
+export default ScoreBox

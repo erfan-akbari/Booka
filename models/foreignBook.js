e2ed5653c343ceb2020c33ba48b58f3bf5b24ecd
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-24 13:55:01 +0330

    Coding foreignbook model

---
+++ models/foreignBook.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+
+const schema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    caption: {
+        type: String,
+        required: true
+    },
+    poster: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5,
+    },
+    discount: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 100,
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    shadow: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    writer: {
+        type: String,
+        required: true
+    },
+    translators: {
+        type: String,
+        required: true
+    },
+    publication: {
+        type: String,
+        required: true
+    }
+})
+
+const model = mongoose.models.foreignbook || mongoose.model('foreignbook', schema)
+
+export default model;

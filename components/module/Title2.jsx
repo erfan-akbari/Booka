cfffe578f7949be2ae787e30f71ffe8a7bf38bce
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-24 13:45:55 +0330

    Create new Component Title2

---
+++ components/module/Title2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Title2({ value }) {
+    return (
+        <h3 className='text-lime-600 text-xl font-bold mb-2 drop-shadow-lg hover:drop-shadow-2xl underline'>
+            {value}
+        </h3>
+    )
+}
+
+export default Title2

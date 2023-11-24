d6a995be5edfe9f04eee421a6737678de13619e1
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-24 13:45:45 +0330

    Create new Component Title

--- components/module/Title.jsx
+++ components/module/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Title({ value }) {
     return (
-        <h2 className='text-red-600 '>
+        <h2 className='text-red-600 text-xl md:text-3xl font-bold my-5 drop-shadow-lg hover:drop-shadow-2xl underline'>
             {value}
         </h2>
     )

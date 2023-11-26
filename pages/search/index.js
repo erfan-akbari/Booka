f5ad961d665e66ea5d2a3152a2e6adee980e2039
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-26 20:13:29 +0330

    fixed error & development Search page

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,20 +1,21 @@
-import { Router, useRouter } from "next/router"
+import { useRouter } from "next/router"
 import { useState } from "react"
 import ContentWrapper from "@/components/module/ContentWrapper"
 import CardV2 from "@/components/module/CardV2"
 
 import connectToDB from "@/utils/db"
 import booksModel from "@/models/book"
+import articlesModel from "@/models/article"
 import CardV4 from "@/components/module/CardV4"
 import Link from "next/link"
 
-export default function SearchResults({ books }) {
-  const { query } = useRouter()
+export default function SearchResults({ books, articles }) {
+  const router = useRouter()
   const [search, setSearch] = useState('')
 
   const EnterHandler = e => {
     if (e.keyCode === 13) {
-      Router.push(`/search?q=${search}`)
+      router.push(`/search?q=${search}`)
     }
   }
 
@@ -48,27 +49,31 @@ export default function SearchResults({ books }) {
         {/* //////// */}
         <h2 className="mb-10 text-2xl font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-5xl">
           جستجو
-          <span className="mr-5 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{query.q}</span>
+          <span className="mr-5 text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{router.query.q}</span>
         </h2>
         {/* //////// */}
         <h2 className="text-indigo-600 text-3xl font-black mb-5">کتاب‌ها</h2>
         <section className='flex flex-col items-center sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-          {books.length === 0 && (
+          {!books.length == 0 ? (
+            books?.map(book => (
+              <CardV2 key={book._id} {...book} />
+            ))
+          ) : (
             <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative w-full" role="alert">
               <p className="font-bold">کتابی یافت نشد</p>
             </div>
           )}
-          {books?.slice(0, 8).map(book => (
-            <CardV2 key={book._id} {...book} />
-          ))}
         </section>
         {/* //////// */}
         <h2 className="text-indigo-600 text-3xl font-black mt-10 mb-5">مقاله ها</h2>
         <section className='flex flex-col items-center sm:grid sm:grid-cols-2  gap-5'>
-          <CardV4 />
-          <CardV4 />
-          <CardV4 />
-          <CardV4 />
+          {!articles.length == 0 ? (
+            articles?.map(article => <CardV4 key={article._id} {...article} /> ) ) : (
+            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative w-full" role="alert">
+              <p className="font-bold">مقاله ای یافت نشد</p>
+            </div>
+          )
+          }
         </section>
         {/* //////// */}
       </ContentWrapper>
@@ -78,13 +83,15 @@ export default function SearchResults({ books }) {
 
 export async function getServerSideProps({ query }) {
   connectToDB()
-
   const { q } = query
+
   const books = await booksModel.find({ title: { $regex: q } })
+  const articles = await articlesModel.find({ title: { $regex: q } })
 
   return {
     props: {
-      books: JSON.parse(JSON.stringify(books))
+      books: JSON.parse(JSON.stringify(books)),
+      articles: JSON.parse(JSON.stringify(articles))
     }
   }
 } 

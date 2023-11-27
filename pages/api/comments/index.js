8b68da41729a50d993cd60350f0d8f48bcf59cfa
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-27 22:24:05 +0330

    Coding api route comments (GET, POST)

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,14 +1,30 @@
 const mongoose = require('mongoose');
-const commentsModel = require('@/models/comment')
+import commentsModel from '@/models/comment'
+import connectToDB from '@/utils/db'
 
-const handler = (req, res) => {
-    const { method } = req
+const handler = async (req, res) => {
+    connectToDB()
 
-    if (method === 'GET') {
-        /// codes
-    } else if (method === 'POST') {
-        /// codes
+    if (req.method === 'GET') {
+        const comments = await commentsModel.find({}, '-__v').populate('creator book')
+        if (comments) {
+            res.json(comments)
+        } else {
+            return res.status(500).json({ message: 'server error' })
+        }
+    } else if (req.method === 'POST') {
+        const { question, creator, book } = req.body
+        if (question && creator && book) {
+            const newComments = await commentsModel.create({ question, creator, book })
+            if (newComments) {
+                res.status(201).json({ newComments, message: 'Comment created successfully' })
+            } else {
+                return res.status(500).json({ message: 'server error' })
+            }
+        } else {
+            res.status(422).json({ message: 'Please submit the body correctly' })
+        }
     }
 }
 
-export default handler
+export default handler;

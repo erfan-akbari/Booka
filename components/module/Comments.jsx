3bde1f367d61a0cdeca2b6cd59b1846f3ce5e6c1
Erfan-Akbari <akbarierfan8004@gmail.com>
2023-11-27 22:20:06 +0330

    Comment send  feature

--- components/module/Comments.jsx
+++ components/module/Comments.jsx
@@ -1,6 +1,71 @@
-import React from 'react'
+import useGetMe from '@/hooks/useGetMe'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
+import swal from 'sweetalert'
 
-function Comments() {
+function Comments({ data }) {
+    const { _id } = data
+    const router = useRouter()
+    const [comment, setComment] = useState('')
+    const { isLoginUser, userData } = useGetMe()
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        if (isLoginUser) {
+            /// codes
+            if (comment) {
+                // codes
+                const res = await fetch(`/api/comments`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        question: comment,
+                        creator: userData._id,
+                        book: _id,
+                    })
+                })
+
+                if (res.status === 201) {
+                    swal({
+                        title: 'نظر شما با موفقیت ثبت شد',
+                        icon: 'success',
+                        button: 'تایید'
+                    })
+                    setComment('')
+                } else {
+                    swal({
+                        title: 'نظر شما با موفقیت ثبت نشد',
+                        icon: 'error',
+                        button: 'تلاش مجدد'
+                    })
+                    setComment('')
+                }
+
+            } else {
+                // codes
+                swal({
+                    title: 'ابتدا نظر خود را تایپ کنید',
+                    icon: 'warning',
+                    button: 'تایید'
+                })
+            }
+        } else {
+            /// codes
+            swal({
+                title: 'ابتدا باید وارد سایت شوید',
+                icon: 'warning',
+                button: 'اکی'
+            }).then(result => {
+                if (result) {
+                    router.push(`/login`)
+                }
+            })
+        }
+
+    }
+
     return (
         <section className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
             <div className=" mx-auto px-4">
@@ -10,11 +75,17 @@ function Comments() {
                 <form className="mb-6">
                     <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                         <label htmlFor="comment" className="sr-only">Your comment</label>
-                        <textarea id="comment" rows="6"
+                        <textarea
+                            id="comment"
+                            rows="6"
                             className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-                            placeholder="تایپ کنید..." required></textarea>
+                            placeholder="تایپ کنید..."
+                            value={comment}
+                            onChange={e => setComment(e.target.value)}
+                            required></textarea>
                     </div>
-                    <button type="submit"
+                    <button
+                        onClick={handleSubmit}
                         className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-rose-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
                         ارسال نظر
                     </button>
@@ -26,7 +97,7 @@ function Comments() {
                                 className="mr-2 w-6 h-6 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
                                 alt="Michael Gough" />Michael Gough</p>
-                            <p className="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-08"
+                            <p className="text-sm text-gray-600 dark:text-gray-400"><time dateTime="2022-02-08"
                                 title="February 8th, 2022">Feb. 8, 2022</time></p>
                         </div>
                         <button id="dropdownComment1Button" data-dropdown-toggle="dropdownComment1"
@@ -58,16 +129,16 @@ function Comments() {
                         </div>
                     </footer>
                     <p className="text-gray-500 dark:text-gray-400">
-                    لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
-                    لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
-                    لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
-                    لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
+                        لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
+                        لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
+                        لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
+                        لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است.
                     </p>
                     <div className="flex items-center mt-4 space-x-4">
                         <button type="button"
                             className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
                             <svg className="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokewidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
                             </svg>
                             Reply
                         </button>
@@ -80,7 +151,7 @@ function Comments() {
                                 className="mr-2 w-6 h-6 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                                 alt="Jese Leos" />Jese Leos</p>
-                            <p className="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-02-12"
+                            <p className="text-sm text-gray-600 dark:text-gray-400"><time dateTime="2022-02-12"
                                 title="February 12th, 2022">Feb. 12, 2022</time></p>
                         </div>
                         <button id="dropdownComment2Button" data-dropdown-toggle="dropdownComment2"
@@ -116,7 +187,7 @@ function Comments() {
                         <button type="button"
                             className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
                             <svg className="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokewidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
                             </svg>
                             Reply
                         </button>
@@ -129,7 +200,7 @@ function Comments() {
                                 className="mr-2 w-6 h-6 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
                                 alt="Bonnie Green" />Bonnie Green</p>
-                            <p className="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-03-12"
+                            <p className="text-sm text-gray-600 dark:text-gray-400"><time dateTime="2022-03-12"
                                 title="March 12th, 2022">Mar. 12, 2022</time></p>
                         </div>
                         <button id="dropdownComment3Button" data-dropdown-toggle="dropdownComment3"
@@ -164,7 +235,7 @@ function Comments() {
                         <button type="button"
                             className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
                             <svg className="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokewidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
                             </svg>
                             Reply
                         </button>
@@ -177,7 +248,7 @@ function Comments() {
                                 className="mr-2 w-6 h-6 rounded-full"
                                 src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
                                 alt="Helene Engels" />Helene Engels</p>
-                            <p className="text-sm text-gray-600 dark:text-gray-400"><time pubdate datetime="2022-06-23"
+                            <p className="text-sm text-gray-600 dark:text-gray-400"><time dateTime="2022-06-23"
                                 title="June 23rd, 2022">Jun. 23, 2022</time></p>
                         </div>
                         <button id="dropdownComment4Button" data-dropdown-toggle="dropdownComment4"
@@ -212,7 +283,7 @@ function Comments() {
                         <button type="button"
                             className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium">
                             <svg className="mr-1.5 w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokewidthe="2" d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z" />
                             </svg>
                             Reply
                         </button>
